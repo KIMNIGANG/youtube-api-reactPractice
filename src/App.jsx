d3558c9384449c999873b9931225da2dedff3d34
kimnigang <zeus2424797@gmail.com>
2021-01-04 01:46:00 +0900

    almost

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import styles from "./App.module.css";
+import Searchheader from "./components/search_header/search_header";
+import VideoList from "./components/video_list/video_list";
+
+function App({ youtube }) {
+  const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
+  };
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
+  }, []);
+
+  return (
+    <div className={styles.app}>
+      <Searchheader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,39 @@
+import React, { useRef } from "react";
+import styles from "./search_header.module.css";
+
+const Searchheader = ({ onSearch }) => {
+  const inputRef = useRef();
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+  };
+  const onKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+  const onClick = () => {
+    handleSearch();
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img className={styles.img} src="/images/logo.png" alt="logo" />
+        <h1 className={styles.title}>YouTube</h1>
+      </div>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="text"
+        placeholder="Search..."
+        onKeyPress={onKeyPress}
+      />
+      <button type="submit" className={styles.button} onClick={onClick}>
+        <img className={styles.buttonImg} src="/images/search.png" alt="" />
+      </button>
+    </header>
+  );
+};
+
+export default Searchheader;
